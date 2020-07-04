ecc9d612d231badd037a3f8d48fb597ba12bc809
Tanmay Anand <tanmayanand@gmail.com>
2020-07-04 14:58:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,8 +64,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (Number.isInteger(ccnumber)==true){
-    return `Your total cost is $${total()}, which will be charged to the card ${ccnumber}.`;
+  if (Number.isInteger(cardNumber)==true){
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
 }
 else{
     return `Sorry, we don't have a credit card on file for you.`;

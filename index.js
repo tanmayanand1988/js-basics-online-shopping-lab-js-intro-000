a31b0b8c1b3e1ceb735d23c3c519b19c06760044
Tanmay Anand <tanmayanand@gmail.com>
2020-07-04 15:18:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,8 +65,9 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (Number.isInteger(cardNumber)==true){
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
-    cart=[];
+let outputstring=`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+cart.splice(0,cart.length);
+return outputstring;
 }
 else{
     return `Sorry, we don't have a credit card on file for you.`;
